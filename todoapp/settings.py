4f13e11bbb7fc53fdacd2a6071bc17e438a5ed5b
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-02 20:48:04 +0300

    added corsheaders

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'todo',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,8 +50,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
 ROOT_URLCONF = 'todoapp.urls'
 
 TEMPLATES = [
